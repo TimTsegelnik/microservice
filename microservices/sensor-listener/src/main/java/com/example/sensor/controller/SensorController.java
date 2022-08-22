ca13cf2df5a3ec031b71dfcbaa019917fa771e60
Tim <timtsegelnik@gmail.com>
2022-08-22 11:26:35 +0300

    "add liquid base to sensor-listener"

--- microservices/sensor-listener/src/main/java/com/example/sensor/controller/SensorController.java
+++ microservices/sensor-listener/src/main/java/com/example/sensor/controller/SensorController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,22 +17,23 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/sensor-listener/v1/sensors")
 @AllArgsConstructor
+@Validated
 public class SensorController {
 
     private final SensorService sensorService;
 
     @GetMapping
     public ResponseEntity<Page<SensorData>> getAllSensors(
-            @RequestParam("page") int page,
-            @RequestParam("size") int size
+            @RequestParam("page")  int page,
+            @RequestParam("size")  int size
     ) {
         return ResponseEntity.ok(sensorService.findAllSensors(page, size));
     }
 
     @GetMapping("/between")
     public ResponseEntity<Page<SensorData>> findSensorsBetween(
-            @RequestParam("page") int page,
-            @RequestParam("size") int size,
+            @RequestParam("page")  int page,
+            @RequestParam("size")  int size,
             @RequestParam("startWith") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startWith,
             @RequestParam("endWith") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endWith
     ) {
