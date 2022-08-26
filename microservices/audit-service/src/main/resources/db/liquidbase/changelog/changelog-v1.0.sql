0e1c97b80258cab8eef08ba85b45b7000d73ac7c
Tim <timtsegelnik@gmail.com>
2022-08-26 19:22:36 +0300

    fix test to service-messenger

--- microservices/audit-service/src/main/resources/db/liquidbase/changelog/changelog-v1.0.sql
+++ microservices/audit-service/src/main/resources/db/liquidbase/changelog/changelog-v1.0.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
 
---changeset Tsegelnik:1.0
+--changeset Admin:1.0
 CREATE SEQUENCE sensor_seq as int8 start 10;
 
---changeset Tsegelnik:1.1
+--changeset Admin:1.1
 CREATE TABLE SENSOR
 (
     id          serial                      not null,
@@ -14,16 +14,16 @@ CREATE TABLE SENSOR
     primary key (id, status)
 ) PARTITION BY LIST (status);
 
---changeset Tsegelnik:1.2
+--changeset Admin:1.2
 CREATE TABLE SENSOR_NORMAL PARTITION OF SENSOR FOR VALUES IN ('NORMAL');
 
---changeset Tsegelnik:1.3
+--changeset Admin:1.3
 CREATE TABLE SENSOR_LOADED PARTITION OF SENSOR FOR VALUES IN ('LOADED');
 
---changeset Tsegelnik:1.4
+--changeset Admin:1.4
 CREATE TABLE SENSOR_FAILED PARTITION OF SENSOR FOR VALUES IN ('FAILED');
 
---changeset Tsegelnik:1.5
+--changeset Admin:1.5
 CREATE OR REPLACE FUNCTION sensor_insert_trigger()
     RETURNS TRIGGER AS
 '
@@ -45,7 +45,7 @@ CREATE OR REPLACE FUNCTION sensor_insert_trigger()
 '
     LANGUAGE plpgsql;
 
---changeset Tsegelnik:1.6
+--changeset Admin:1.6
 CREATE TRIGGER insert_measurement_trigger
     BEFORE INSERT OR UPDATE
     ON SENSOR
