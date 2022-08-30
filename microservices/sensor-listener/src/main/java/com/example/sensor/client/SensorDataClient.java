3197239f0659e404771e51ac0d4be5cf1f2a4ee0
Tim <timtsegelnik@gmail.com>
2022-08-30 13:10:05 +0300

    add validation to sensor-listener client

--- microservices/sensor-listener/src/main/java/com/example/sensor/client/SensorDataClient.java
+++ microservices/sensor-listener/src/main/java/com/example/sensor/client/SensorDataClient.java
@@ -3,12 +3,16 @@ package com.example.sensor.client;
 
 import com.example.sensor.sensorDao.SensorMetricsDao;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.validation.Valid;
+
+@Validated
 @FeignClient(name = "sensor", url = "http://sensor:8088")
 public interface SensorDataClient {
 
-
+    @Valid
     @GetMapping("/sensor/v1/data")
     SensorMetricsDao getSensorMetrics();
 }
